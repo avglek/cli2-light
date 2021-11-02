66e45cc6a38a2205c56567d6c9e19a23138bfe75
avglek <avglek@gmail.com>
2021-11-02 19:46:40 +0300

    test git

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
-import { createEpicMiddleware } from 'redux-observable'
-import './index.css'
-import 'smart-webcomponents-react/source/styles/smart.default.css'
-import App from './App'
-import { rootReducer } from './store/reducers/rootReducer'
-import { rootEpic } from './store/epics/rootEpics'
-import ErrorBoundary from './hoc/ErrorBaundary'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { createEpicMiddleware } from 'redux-observable';
+import './index.css';
+import 'smart-webcomponents-react/source/styles/smart.default.css';
+import App from './App';
+import { rootReducer } from './store/reducers/rootReducer';
+import { rootEpic } from './store/epics/rootEpics';
+import ErrorBoundary from './hoc/ErrorBaundary';
 
-const epicMiddleware = createEpicMiddleware()
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const epicMiddleware = createEpicMiddleware();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const baseUrl = '/' ///window.location.pathname
+const baseUrl = '/'; //window.location.pathname
 //const baseUrl = '/test/cli2-light/build/'
 
 const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(epicMiddleware))
-)
+);
 
-epicMiddleware.run(rootEpic)
+epicMiddleware.run(rootEpic);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -34,7 +34,7 @@ ReactDOM.render(
   </Provider>,
 
   document.getElementById('root')
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
